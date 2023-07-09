8609d70a87c2c4e39700535ffe24aa1090750b8d
snorq2 <81600811+snorq2@users.noreply.github.com>
2023-07-09 12:51:58 -0700

    Gauss working, added kmeans

---
+++ gaussian.py
@@ -0,0 +1,66 @@
+from PIL import Image
+import numpy as np
+
+g_filter_3x3 = (1/16) * np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]])
+g_filter_5x5 = (1/273) * np.array([[1, 4, 7, 4, 1], [4, 16, 26, 16, 4], [7, 26, 41, 26, 7], [4, 16, 26, 16, 4], [1, 4, 7, 4, 1]])
+
+g_filter_gx = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+g_filter_gy = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+
+img1 = Image.open('filter1_img.jpg')
+img2 = Image.open('filter2_img.jpg')
+
+# img1.show()
+# img2.show()
+
+filt_img1 = np.array(img1)
+filt_img2 = np.array(img2)
+
+print(filt_img1)
+
+
+def gaussian_sample(sample: np.ndarray, filter: np.ndarray):
+    if np.shape(sample) != np.shape(filter):
+        raise AttributeError("Sample must have same shape as filter")
+    
+    output = sample * filter
+    # output = round(np.sum(output))
+    # return output
+    
+    return np.sum(output)
+
+def gaussian_iterate(source: np.ndarray, filter: np.ndarray):
+    padding = int((len(filter)-1) / 2)
+    output = np.zeros(np.shape(source))
+    prepped = np.pad(source, padding)
+    print(output)
+    for i in range(0, len(output)):
+        for j in range(0, len(output[0])):
+            output[i, j] = gaussian_sample(prepped[i:i+(padding*2)+1, j:j+(padding*2)+1], filter)
+    return output
+    
+def apply_gaussian(source, filter):
+    img = np.array(Image.open(source))
+    return Image.fromarray(gaussian_iterate(img, filter))
+
+def apply_sobel(source):
+    img = np.array(Image.open(source))
+    gx = gaussian_iterate(img, g_filter_gx)
+    gy = gaussian_iterate(img, g_filter_gy)
+    gx = gx * gx
+    gy = gy * gy
+    g = np.sqrt(gx + gy)
+    return Image.fromarray(g)
+    
+    
+# gaus_1 = apply_gaussian('filter1_img.jpg', g_filter_3x3)
+# gaus_1_5x5 = apply_gaussian('filter1_img.jpg', g_filter_5x5)
+# gaus_1_gx = apply_gaussian('filter1_img.jpg', g_filter_gx)
+# gaus_1_gy = apply_gaussian('filter1_img.jpg', g_filter_gy)
+# img1.show()
+# gaus_1.show()
+# gaus_1_5x5.show()
+# gaus_1_gx.show()
+# gaus_1_gy.show()
+sobel_1 = apply_sobel('filter1_img.jpg')
+sobel_1.show()
---
+++ kmeans.py
@@ -0,0 +1,147 @@
+import numpy as np
+import sys
+
+# Preset parameters for clusters and number of runs
+num_k = 5
+r = 10
+
+# Set up data
+cluster_data_preprop = np.loadtxt("545_cluster_dataset (1).txt")
+
+cluster_data = np.zeros(
+    (len(cluster_data_preprop), len(cluster_data_preprop[0]) + 1))
+cluster_data[:, :-1] = cluster_data_preprop
+
+# Calculate L2 distances
+# Input is a data point sliced to x/y coordinates and a single centroid
+# Returns distance between data point and centroid
+
+
+def L2(data, centroid):
+    pretotal = (data - centroid) ** 2
+    sumtotal = np.sum(pretotal)
+    return np.sqrt(sumtotal)
+
+# Identify closest centroid
+# Input is data point including its cluster assignment element and the centroid array
+# Output is the data slice with updated cluster assignment
+
+
+def calc_closest(data, centroid):
+    last_distance = sys.float_info.max
+    for i in range(0, num_k):
+        current_distance = L2(data[0:2], centroid[i]) ** 2
+        if last_distance > current_distance:
+            last_distance = current_distance
+            data[2] = i
+    return data
+
+# Iterate through data array to update cluster assignments
+# Inputs are data full data array and the centroid array
+# Output is the data array with updated cluster assignments
+
+
+def calc_distance(data, centroid):
+    for i in range(0, len(data)):
+        data[i] = calc_closest(data[i, :], centroid)
+    return data
+
+# Centroid initializer
+# Input is data array
+# Output is num_k random set of x/y coordinate slices
+
+
+def randomize_centroids(data):
+    random = np.random.randint(0, high=len(data), size=num_k)
+    return data[random, :-1]
+
+# Compute centroids based on current data membership
+# Input is data array
+# Output is updated centroid array
+
+
+def compute_centroids(data):
+    centroids = np.zeros((num_k, 2))
+    num_points = np.zeros((num_k, 1))
+    for i in range(0, len(data)):
+        centroids[int(data[i, 2])][0] = centroids[int(
+            data[i, 2])][0] + data[i, 0]
+        centroids[int(data[i, 2])][1] = centroids[int(
+            data[i, 2])][1] + data[i, 1]
+        num_points[int(data[i, 2])] += 1
+    return centroids / num_points
+
+# Calculate MSE for current data and centroids
+# Input is data array with cluster assignments and centroids array
+# Output is MSE
+
+
+def WCSS(data, centroids):
+    sum = 0
+    for i in range(0, len(data)):
+        sum = sum + (L2(data[i, 0:2], centroids[int(data[i, 2])]) ** 2)
+
+    return sum
+
+# Primary EM loop for one run of K-Means
+# Input is starting data array
+# Output is final MSE, resulting centroid array, and resulting data array with cluster assignments
+# Uses double-lookback on centroids to avoid a centroid oscillation issue
+# Terminates if the current centroid array match either of the previous two centroid arrays
+# Includes a catch to return early if a centroid drops out
+
+
+def kmeans_run(data):
+    centroid_start = randomize_centroids(data)
+    centroid_last = np.zeros(centroid_start.shape)
+    centroid_last_2 = np.zeros(centroid_start.shape)
+    repeat = True
+    while (repeat):
+        centroid_last_2 = centroid_last
+        centroid_last = centroid_start
+        data = calc_distance(data, centroid_start)
+        centroid_start = compute_centroids(data)
+        if (np.isnan(centroid_start).any()):
+            return 0, centroid_start, data
+
+        if (np.array_equal(centroid_start, centroid_last, True) == True):
+            repeat = False
+        if (np.array_equal(centroid_start, centroid_last_2, True) == True):
+            repeat = False
+
+    run_WCSS = WCSS(data, centroid_start)
+
+    return run_WCSS, centroid_start, data
+
+# Filename helper to generate a file name based on current parameters
+
+
+def generate_filename(type):
+    return "kmeans-k" + str(num_k) + "-" + type + ".csv"
+
+
+# Initialize arrays to capture data for each run
+all_WCSS = np.zeros(r)
+all_centroids = np.zeros((r, num_k, 2))
+all_data = np.zeros((r, len(cluster_data), len(cluster_data[0])))
+i = 0
+
+# Primary loop for each run
+# Includes a check for a vanished centroid, restarting a run if one is detected
+while i < r:
+    all_WCSS[i], all_centroids[i], all_data[i] = kmeans_run(cluster_data)
+    if (np.isnan(all_centroids[i]).any()):
+        print("Found NaN on run " + str(i) + ", restarting")
+    else:
+        print("k-Means run " + str(i))
+        print("WCSS: " + str(all_WCSS[i]))
+        print("Centroids: " + str(all_centroids[i]))
+        i += 1
+
+# Find lowest MSE and save that data
+best_run = np.argmin(all_WCSS)
+np.savetxt(generate_filename("centroids"),
+           all_centroids[best_run], delimiter=",", fmt="%f")
+np.savetxt(generate_filename("dataset"),
+           all_data[best_run], delimiter=",", fmt="%f")
+print("Best WCSS was on run " + str(best_run))
